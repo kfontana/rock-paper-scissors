44f2b768aa8abe67d1b0bc27b9025affbfca6172
Kevin <kevinfontana1993@gmail.com>
2024-03-13 12:28:37 -0300

    Modify logic and interaction with elements.

--- script.js
+++ script.js
@@ -15,35 +15,45 @@ function playGame(){
     let score = 0;
 
     function playRound(playerSelection, computerSelection){
-        playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
         computerSelection = getComputerChoice();
-        if (playerSelection === "rock" && computerSelection === "Rock" || playerSelection === "paper" && computerSelection === "Paper" || playerSelection === "scissors" && computerSelection === "Scissors"){
-            console.log("It's a tie! Your choice was: " + playerSelection + " and the computer choice was: " + computerSelection );
-        }else if(playerSelection === "rock" && computerSelection === "Scissors" || playerSelection === "paper" && computerSelection === "Rock"){
+        if (playerSelection === "Rock" && computerSelection === "Rock" || playerSelection === "Paper" && computerSelection === "Paper" || playerSelection === "Scissors" && computerSelection === "Scissors"){
+        }else if(playerSelection === "Rock" && computerSelection === "Scissors" || playerSelection === "Paper" && computerSelection === "Rock"){
             score += 1;
-            console.log("You won! Your choice was: " + playerSelection + " and the computer choice was: " + computerSelection );
         }else{
             score -= 1;
-            console.log("You lose! Your choice was: " + playerSelection + " and the computer choice was: " + computerSelection)
         }
+
+        const scoreDiv = document.querySelector('#score');
+        scoreDiv.textContent = "Score: " + score + " (wins who reach to 5 first.)";
+
+        if(score === 5){
+            scoreDiv.textContent = "You won the game! Congrats! Your score was " + score;
+            score = 0;
+        }else if(score === -5){
+            scoreDiv.textContent = "You lose. Better luck next time. Your score was " + score;
+            score = 0;
+        }
+    
         return score;
     }
-    
-    playRound();  
-    playRound(); 
-    playRound(); 
-    playRound(); 
-    playRound();   
 
-    if(score > 0){
-        document.write("You won the game! Congrats! Your score was " + score);
-    }else if(score = 0){
-        document.write("It's a tie! Keep trying. Your score was " + score);
-    }else{
-        document.write("You lose. Better luck next time. Your score was " + score);
-    }
+    const rockButton = document.querySelector('#rock');
 
-    /* Check negative numbers in a lose round. */
+    rockButton.addEventListener('click', () => {
+        playRound('Rock');
+    });   
+
+    const paperButton = document.querySelector('#paper');
+
+    paperButton.addEventListener('click', () => {
+        playRound('Paper');
+    });   
+
+    const scissorsButton = document.querySelector('#scissors');
+
+    scissorsButton.addEventListener('click', () => {
+        playRound('Scissors');
+    });   
 }
 
 playGame();
